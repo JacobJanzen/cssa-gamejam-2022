9ec0235a235428f667d711c583a2405860f19f16
Mohsen Yousefian <me@manzik.com>
2022-01-29 16:32:24 -0600

    Added automatic scene height calculation

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -7,21 +7,29 @@ public class CameraControl : MonoBehaviour
 {
     public Transform player;
     public Vector3 offset;
-    public float maxHeight;
+    public float baseAltitude = 0;
+    private float sceneHeight;
+    private Material bgMaterial;
     // Start is called before the first frame update
     void Start()
     {
-        Skybox skybox = this.GetComponent<Skybox>();
-        
+        bgMaterial = this.GetComponent<Skybox>().material;
+        // Calculate the scene's bounds 
+        Bounds b = new Bounds();
+        foreach (Renderer r in FindObjectsOfType<Renderer>())
+        {
+            b.Encapsulate(r.bounds);
+        }
+        sceneHeight = b.max.y;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         transform.position = new Vector3(player.position.x + offset.x, player.position.y + offset.y + 1, offset.z); // Camera follows the player with specified offset position
-        Skybox skybox = this.GetComponent<Skybox>();
-        skybox.material.SetFloat("_Altitude", player.position.y);
-        skybox.material.SetFloat("_PlayerX", player.position.x);
-        skybox.material.SetFloat("_MaxAltitude", 27f);
+        
+        bgMaterial.SetFloat("_Altitude", player.position.y + baseAltitude);
+        bgMaterial.SetFloat("_PlayerX", player.position.x);
+        bgMaterial.SetFloat("_MaxAltitude", sceneHeight + baseAltitude);
     }
 }
