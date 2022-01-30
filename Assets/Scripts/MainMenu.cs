732a47cb101ba8a69f3370be602b463ab726779c
Jacob Janzen <jacob.a.s.janzen@gmail.com>
2022-01-30 11:31:20 -0600

    main menu sends player to correct scene

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {
     public void StartGame()
     {
-        SceneManager.LoadScene("GroundLevel");
+        SceneManager.LoadScene("CombinedPlatforms");
         Debug.Log("Game Started");
     }
 
